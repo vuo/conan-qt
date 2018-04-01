8c8a37a99f1a791bfca144ed1922f006523da134
smokris <smokris@softpixel.com>
2018-04-01 17:01:46 -0400

    Enable test_package to run on Linux (for now, disable the dylib tests).

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -14,34 +14,33 @@ class QtTestConan(ConanFile):
         self.copy('*', src='lib', dst='lib')
         self.copy('*', src='plugins', dst='plugins')
 
-        # Conan's `self.copy` doesn't copy symlinks to folders, so copy it ourselves.
-        shutil.copytree(src='%s/lib' % self.deps_cpp_info["qt"].rootpath, dst='lib-qt', symlinks=True)
-        shutil.rmtree('lib/QtCore.framework')
-        shutil.move(src='lib-qt/QtCore.framework', dst='lib/QtCore.framework')
-        shutil.rmtree('lib-qt')
+        if platform.system() == 'Darwin':
+            # Conan's `self.copy` doesn't copy symlinks to folders, so copy it ourselves.
+            shutil.copytree(src='%s/lib' % self.deps_cpp_info["qt"].rootpath, dst='lib-qt', symlinks=True)
+            shutil.rmtree('lib/QtCore.framework')
+            shutil.move(src='lib-qt/QtCore.framework', dst='lib/QtCore.framework')
+            shutil.rmtree('lib-qt')
 
     def test(self):
-        self.run('qbs run')
+        self.run('qbs run -f "%s"' % self.source_folder)
 
         # Ensure we only link to system libraries.
-        for f in [
-            'lib/QtConcurrent.framework/QtConcurrent',
-            'lib/QtCore.framework/QtCore',
-            'lib/QtGui.framework/QtGui',
-            'lib/QtMacExtras.framework/QtMacExtras',
-            'lib/QtNetwork.framework/QtNetwork',
-            'lib/QtOpenGL.framework/QtOpenGL',
-            'lib/QtPrintSupport.framework/QtPrintSupport',
-            'lib/QtScript.framework/QtScript',
-            'lib/QtSql.framework/QtSql',
-            'lib/QtSvg.framework/QtSvg',
-            'lib/QtTest.framework/QtTest',
-            'lib/QtWidgets.framework/QtWidgets',
-            'lib/QtXml.framework/QtXml',
-            'plugins/platforms/libqcocoa.dylib',
-        ]:
-            if platform.system() == 'Darwin':
+        if platform.system() == 'Darwin':
+            for f in [
+                'lib/QtConcurrent.framework/QtConcurrent',
+                'lib/QtCore.framework/QtCore',
+                'lib/QtGui.framework/QtGui',
+                'lib/QtMacExtras.framework/QtMacExtras',
+                'lib/QtNetwork.framework/QtNetwork',
+                'lib/QtOpenGL.framework/QtOpenGL',
+                'lib/QtPrintSupport.framework/QtPrintSupport',
+                'lib/QtScript.framework/QtScript',
+                'lib/QtSql.framework/QtSql',
+                'lib/QtSvg.framework/QtSvg',
+                'lib/QtTest.framework/QtTest',
+                'lib/QtWidgets.framework/QtWidgets',
+                'lib/QtXml.framework/QtXml',
+                'plugins/platforms/libqcocoa.dylib',
+            ]:
                 self.run('! (otool -L ' + f + ' | tail +3 | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
                 self.run('! (otool -l ' + f + ' | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
-            elif platform.system() == 'Linux':
-                self.run('! (ldd ' + f + ' | grep -v "^lib/" | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
